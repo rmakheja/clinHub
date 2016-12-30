11d8b89dc819e4c7c0f83fb0bed519cc2b1af680
rmakheja <rmakheja@cs.stonybrook.edu>
2016-12-30 10:20:17 -0500

    Initial
    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var src = "https://cdn.firebase.com/js/client/2.4.2/firebase.js"
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function googleSignin() {
+   firebase.auth()
+   
+   .signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+		
+      console.log(token)
+      console.log(user)
+   }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+		
+      console.log(error.code)
+      console.log(error.message)
+   });
+}
+
+function googleSignout() {
+   firebase.auth().signOut()
+	
+   .then(function() {
+      console.log('Signout Succesfull')
+   }, function(error) {
+      console.log('Signout Failed')  
+   });
+}
